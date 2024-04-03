a03ae444498a9667571faebe7eff6ae5cf42edf0
allyst <alastroboy2000@gmail.com>
2024-04-03 11:16:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,47 @@
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 from logger import LogManagement
-from os import path
+import operator
+from functools import partial
  
 class MeuApp(QMainWindow):
     num1 = 0
     num2 = 0
     numResult = 0
     op = None
+    operacoes = {
+        "+": operator.add, "-": operator.sub,
+        "*": operator.mul, "/": operator.truediv,
+    }
  
     def __init__(self):
         super().__init__()
         loadUi('calculadora.ui', self)
-        self.setAcoes()
  
+        for i in range(10):
+            btn = getattr(self, f'btn{i}')
+            btn.clicked.connect( partial(self.btnClicado, btn) )
  
-    def setAcoes(self):
-        self.Botao_0.clicked.connect(lambda: self.btnClicado(self.Botao_0))
-        self.Botao_1.clicked.connect(lambda: self.btnClicado(self.Botao_1))
-        self.Botao_2.clicked.connect(lambda: self.btnClicado(self.Botao_2))
-        self.Botao_3.clicked.connect(lambda: self.btnClicado(self.Botao_3))
-        self.Botao_4.clicked.connect(lambda: self.btnClicado(self.Botao_4))
-        self.Botao_5.clicked.connect(lambda: self.btnClicado(self.Botao_5))
-        self.Botao_6.clicked.connect(lambda: self.btnClicado(self.Botao_6))
-        self.Botao_7.clicked.connect(lambda: self.btnClicado(self.Botao_7))
-        self.Botao_8.clicked.connect(lambda: self.btnClicado(self.Botao_8))
-        self.Botao_9.clicked.connect(lambda: self.btnClicado(self.Botao_9))
         self.virgula.clicked.connect(lambda: self.btnClicado(self.virgula))
        
-        self.add.clicked.connect(lambda: self.definirOperacao(self.add))
-        self.subtrair.clicked.connect(lambda: self.definirOperacao(self.subtrair))
-        self.multi.clicked.connect(lambda: self.definirOperacao(self.multi))
-        self.divisor.clicked.connect(lambda: self.definirOperacao(self.divisor))
+        self.add.clicked.connect(lambda: self.definirOperacao("+"))
+        self.subtrair.clicked.connect(lambda: self.definirOperacao("-"))
+        self.multi.clicked.connect(lambda: self.definirOperacao("*"))
+        self.divisor.clicked.connect(lambda: self.definirOperacao('/'))
  
-        self.porcentagem.clicked.connect(self.porcentagem)
-        self.combo.clicked.connect(self.combo)
- 
-        self.valor.clicked.connect(self.mostraResultado)
         self.allClear.clicked.connect(self.limparDisplay)
+        self.combo.clicked.connect(self.inverter)
+        self.porcentagem.clicked.connect(self.calcPorcentagem)
+        self.outputLabel.clicked.connect(self.mostraResultado)
        
    
     def mostrarDisplay(self, value):
         value = str(value).replace('.', ',')
-        self.valor.setText( value )
+        self.outputLabel.setText( value )
  
  
     def pegarDisplay(self):
-        value = self.valor.text()
+        value = self.outputLabel.text()
         value = value.replace(',', '.')
         try:value = int(value)
         except:value = float(value)
@@ -67,34 +62,14 @@ class MeuApp(QMainWindow):
                     ultimoValor = ''
             # Se for numero float
             else:
-                if self.valor.text()[-1] == ",":
-                    ultimoValor = self.valor.text()
+                if self.outputLabel.text()[-1] == ",":
+                    ultimoValor = self.outputLabel.text()
         self.mostrarDisplay(ultimoValor + btn.text())
  
  
-    def adicao(self):
-        print(f'Soma({self.num1}+{self.num2}) = ', end='')
-        return self.num1 + self.num2
- 
- 
-    def subtracao(self):
-        print(f'Sub({self.num1} - {self.num2})= ', end='')
-        return self.num1 - self.num2
- 
- 
-    def multiplicacao(self):
-        print(f'Mult({self.num1} * {self.num2})= ', end='')
-        return self.num1 * self.num2
- 
- 
-    def divisao(self):
-        print(f'Div({self.num1} / {self.num2})= ', end='')
-        return self.num1 / self.num2
-   
- 
-    def porcentagem(self):
+    def calcPorcentagem(self):
         percento = self.pegarDisplay() / 100
-        if self.op == self.adicao or self.op == self.subtracao:
+        if self.op == operator.add or self.op == operator.sub:
             percento = self.num1 * percento
         self.mostrarDisplay(percento)
  
@@ -106,7 +81,7 @@ class MeuApp(QMainWindow):
  
  
     def definirOperacao(self, operacao):
-        self.op = operacao
+        self.op = self.operacoes[operacao]
         self.num1 = self.pegarDisplay()
         self.num2 = 0
         self.mostrarDisplay(0)
@@ -115,7 +90,7 @@ class MeuApp(QMainWindow):
     def resultado(self):
         if self.op:
             self.num2 = self.pegarDisplay()
-            return self.op()
+            return self.op(self.num1, self.num2)
         else:
             print('nao tem operacao feita')
    
@@ -127,9 +102,9 @@ class MeuApp(QMainWindow):
             else:
                 self.num2 = self.pegarDisplay()
  
-            self.numResult = self.op()
+            self.numResult = self.op(self.num1, self.num2)
             self.mostrarDisplay(self.numResult)
-            print(self.numResult)
+            print(f"({self.num1}, {self.num2}): {self.numResult}")
        
  
     def limparDisplay(self):
@@ -140,11 +115,10 @@ class MeuApp(QMainWindow):
         self.mostrarDisplay(0)
  
  
-    #def localPath(self, relativo):
-        #return f'{path.dirname(path.realpath(__file__))}\\{relativo}'
-    
+ 
 if __name__ == '__main__':
     app = QApplication([])
     window = MeuApp()
     window.show()
     app.exec_()
+ 
